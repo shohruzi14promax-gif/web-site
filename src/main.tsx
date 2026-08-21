b0c4c178ee96220764de38d585a6bda68c83fe51
shohruzi14promax-gif <shohruzi14promax@gmail.com>
2026-08-21 22:16:53 +0500

    ux: remove duplicate floating theme control

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import ThemeMode from './components/ThemeMode';
 import { I18nProvider } from './i18n';
 import './index.css';
 import './styles/production-hardening.css';
@@ -16,9 +15,6 @@ createRoot(root).render(
   <StrictMode>
     <I18nProvider>
       <App />
-      <div className="fixed bottom-5 left-1/2 z-[110] -translate-x-1/2 rounded-2xl bg-slate-950/90 p-1 shadow-xl backdrop-blur-xl">
-        <ThemeMode />
-      </div>
     </I18nProvider>
   </StrictMode>
 );
